3b367c2a002268805f9f507b81b6a0f1a0132e81
Sanhe Hu <husanhe@gmail.com>
2023-04-03 01:03:25 -0400

    Update cookiecutterize.py

--- cookiecutterize.py
+++ cookiecutterize.py
@@ -10,16 +10,16 @@ if dir_tmp.exists():
 dir_tmp.mkdir(parents=True, exist_ok=True)
 
 maker = Maker.new(
-    input_dir="/Users/sanhehu/Documents/CodeCommit/aws_hil-project",
+    input_dir="/Users/sanhehu/Documents/CodeCommit/ami_backup-project",
     output_dir=dir_tmp,
     mapper=[
-        ("aws_hil", "package_name"),
+        ("ami_backup", "package_name"),
         ("Sanhe Hu", "author_name"),
         ("sanhehu@amazon.com", "author_email"),
         ("0.1.1", "semantic_version"),
-        ("aws_data_lab_travelers_us_east_2", "aws_profile"),
-        ("931365232219", "aws_account_id"),
-        ("us-east-2", "aws_region"),
+        ("awshsh_app_dev_us_east_1", "aws_profile"),
+        ("807388292768", "aws_account_id"),
+        ("us-east-1", "aws_region"),
     ],
     include=[],
     exclude=[
