50b64e294ea501faeffffc0f87c3b8442aa495f6
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-04 18:48:16 -0400

    add api module

--- cookiecutter_maker/__init__.py
+++ cookiecutter_maker/__init__.py
@@ -7,8 +7,3 @@ __license__ = "MIT"
 __author__ = "Sanhe Hu"
 __author_email__ = "husanhe@gmail.com"
 __github_username__ = "MacHu-GWU"
-
-try:
-    from .maker import Maker
-except ImportError:
-    pass
