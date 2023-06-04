5af9da4a5e7a32cb6bd4f5585f8946537db9ecd5
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-04 19:56:43 -0400

    add new features

--- tests/my_package-project/my_package/__init__.py
+++ tests/my_package-project/my_package/__init__.py
@@ -7,3 +7,6 @@ my_package is a sample cookiecutter_maker project.
 name = "name"
 str_template = f"{{name}}"
 assert str_template.format(name="alice") == "alice"
+
+author_name = "john.doe"
+author_email = "john.doe@email.com"
