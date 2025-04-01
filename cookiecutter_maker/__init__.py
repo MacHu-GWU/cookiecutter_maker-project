a8fc1b9b9cb84908c3bbdeb4abaa7e2e99863cfb
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-04-01 15:38:00 -0400

    [no ci]

--- cookiecutter_maker/__init__.py
+++ cookiecutter_maker/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from ._version import __version__
-
-__short_description__ = "Convert any git repo into a cookiecutter projects template."
-__license__ = "MIT"
-__author__ = "Sanhe Hu"
-__author_email__ = "husanhe@gmail.com"
-__github_username__ = "MacHu-GWU"
+from ._version import __short_description__
+from ._version import __license__
+from ._version import __author__
+from ._version import __author_email__
+from ._version import __maintainer__
+from ._version import __maintainer_email__
